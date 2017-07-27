2dd2f9946c9c64ec2c12d6097c43a6dd4baa5fbe
badlabor <badlabor@work.com>
2017-07-27 20:24:35 +0800

    继续导出go

---
+++ export_go.go
@@ -0,0 +1,22 @@
+package main
+
+/*
+	导出go格式：
+		type MessageA struct {
+			a int
+			b float
+			c string
+		}
+		func (msg *MessageA) Read(r io.Reader) {
+			a = r.ReadInt()
+			b = r.ReadFloat()
+			c = r.ReadString()
+		}
+		func (msg* MessageA) Write(w io.Writer) {
+			r.WriteInt(a)
+			r.WriteFloat(b)
+			r.WriteString(c)
+		}
+
+
+*/
